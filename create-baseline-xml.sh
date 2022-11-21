086006881a7ca04a99e1bf35bfabda578473d86b
Julian Reschke <julian.reschke@gmx.de>
2022-11-21 13:59:55 +0100

    initial

---
+++ create-baseline-xml.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+REPO=repo/jackrabbit-oak
+USETAG=${TAG-trunk}
+set -x
+ 
+if [ ! -d repo ] ; then
+  rm -rfv repo
+  mkdir repo
+  (cd repo && git clone git@github.com:apache/jackrabbit-oak.git)
+fi
+
+(cd $REPO && git checkout trunk && git checkout . && git pull && git checkout $USETAG && mvn clean install -DskipTests && mvn dependency:tree && mvn dependency:tree > dependencies.txt && mvn clean && git log -n1 > version.txt)
+
+echo "<root>" >> $$.xml
+for i in $(find $REPO/*/target -name baseline.xml) ; do
+  cat $i >> $$.xml
+done
+echo "</root>" >> $$.xml
+
+xmllint --format $$.xml -o baseline.xml
+rm -f $$.xml
+ 
+mv $REPO/dependencies.txt .
+mv $REPO/version.txt .
+
+COMMENT=$(printf "%s - %s" $USETAG $(head -1 version.txt))
+
+# no auto-commit
+echo git commit -a -m "$COMMENT"
